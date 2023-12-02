1cb7fe96ca59b15cd6eb844a9769229238cde722
Ederson <edersonfreitas2012@hotmail.com>
2023-12-02 01:26:03 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/EdersonSouza02/ApiRestGolang-Oppotunity.git/config"
 	"github.com/EdersonSouza02/ApiRestGolang-Oppotunity.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config inicialization error:%v", err)
+		return
+	}
+	// Initialize Router
 	router.Inicialize()
 
 }
